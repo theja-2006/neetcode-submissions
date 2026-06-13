cad8cf991658ba730bc8f3eefacc01617320c83d
Theja B Nair <thejabnair14@gmail.com>
2026-06-13 17:06:56 +0530

    Add: is-palindrome - submission-29

---
+++ Data Structures & Algorithms/is-palindrome/submission-29.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0, j=s.size()-1;
+        while(i<=j)
+        {
+            if(isalnum(s[i]))
+            {   
+                if(isalnum(s[j]))
+                {
+                    if(tolower(s[i])!=tolower(s[j]))
+                        return false;
+                    else
+                    {
+                        i++;
+                        j--;
+                    }
+                }
+                else
+                    j--;
+            }
+            else
+                i++;
+        }
+        return true;
+    }
+};
