f9d94422dad563a4bd1e1f8d9b346a3c986a4a25
Theja B Nair <thejabnair14@gmail.com>
2026-07-01 07:05:34 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int>window;
+        int n=nums.size();
+        int l=0;
+        for(int r=0;r<n;r++)
+        {
+            if(r-l >k)
+            {
+                window.erase(nums[l]);
+                l++;
+            }
+            if(window.count(nums[r]))
+                return true;
+            else
+                window.insert(nums[r]);
+        }
+        return false;
+    }
+};
